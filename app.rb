2697584bfb5c8d75c7056dcc9deaf326260b6aed
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 14:11:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,10 +37,10 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    session[:user_id] == 1
+    session[:user_id] = 1
     #set session id here
     
-    if session[:user_id] == 1
+    if session[:user_id] = 1
       # "Session ID set. It's currently set to #{session[:user_id]}."
       redirect '/fetch_session_id'
     else
